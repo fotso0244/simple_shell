cb8a96de294ee47801500d84398f5fd03ba8ce94
aristide notouom <fotso0244@gmail.com>
2022-06-16 10:40:18 -0700

    My first custom shell

--- simple_shell.c
+++ simple_shell.c
@@ -7,50 +7,57 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <signal.h>
+/**
+ * signalhandler - handler of a signal
+ * @signum: a signal
+ */
+void signalhandler(int signum)
+{printf("toto = %d\n",signum);
+	
+}
 /**
  * simple_shell - a custom shell
  *
- * Return: 0 if success, otherwise -1
  */
-int simple_shell(void)
+void simple_shell(void)
 {
-	char *buf, *argument[] = {"", NULL}, *buf2;
-	pid_t proc;
+	char *buf, *argument[] = {"", NULL};
+	pid_t proc1;
 	int i = 0;
-
-	buf = malloc(sizeof(*buf) * 25);
-	buf2 = malloc(sizeof(*buf2) * 25);
-	if (buf == NULL || buf2 == NULL)
+	char c;
+	
+	signal(SIGINT, signalhandler);
+	while (1)
 	{
-		perror("Malloc error");
-		return (-1);
-	}
-	proc = fork();
-	if (proc == -1)
-	{
-		perror("fork error");
-		return (-1);
-	}
-	if (proc == 0)
-	{
+		if (buf != NULL)
+			free(buf);
+		buf = malloc(sizeof(*buf) * 25);
+		if (buf == NULL)
+			perror("Malloc error");
 		write(1, "#cisfun$ ", 9);
-		read(0, buf, 25);
-		while (buf[i] != '\n')
+		proc1 = fork();
+		while (c != '\n')
 		{
-			buf2[i] = buf[i];
+			read(0, &c, 1);
+			if (c == '\n')
+				break;
+			buf[i] = c;
 			i++;
 		}
-		execve(buf2, argument, NULL);
-		write(2, "./shell", 7);
-		write(2, ": No such file or directory\n", 28);
+		proc1 = fork();
+		if (proc1 == -1)
+			perror("fork error");
+		if (proc1 == 0)
+		{
+			execve(buf, argument, 0);
+			write(1,"./shell: No such file or directory\n", 35);
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
-	else
-	{
-		wait(NULL);
-		free(buf);
-		free(buf2);
-	}
-	return (0);
 }
 /**
  * simple_shell2 - a custom shell
